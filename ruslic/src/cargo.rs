71c66dbd19812f678aeb5b03d16f96e936d7aec0
Jonas <jonas.fiala.cardiff@gmail.com>
2023-01-21 12:05:45 +0100

    Bugfix and create `cargo russol`

---
+++ ruslic/src/cargo.rs
@@ -0,0 +1,68 @@
+use serde::Deserialize;
+
+fn main() {
+    if let Err(code) = process(std::env::args().skip(1)) {
+        std::process::exit(code);
+    }
+}
+
+fn process<I>(args: I) -> Result<(), i32>
+where
+    I: Iterator<Item = String>,
+{
+    let mut russol_rustc_path = std::env::current_exe()
+        .expect("current executable path invalid")
+        .with_file_name("ruslic");
+    if cfg!(windows) {
+        russol_rustc_path.set_extension("exe");
+    }
+
+    // Remove the "russol" argument when `cargo-russol` is invoked as
+    // `cargo --cflag russol` (note the space in `cargo russol` rather than a `-`)
+    let args = args.skip_while(|arg| arg == "russol");
+
+    let exit_status = std::process::Command::new("cargo")
+        .arg("check")
+        .args(args)
+        .env("RUST_TOOLCHAIN", get_rust_toolchain_channel())
+        .env("RUSTUP_TOOLCHAIN", get_rust_toolchain_channel())
+        .env("RUSTC_WRAPPER", russol_rustc_path)
+        .env("SUSLIK_DIR", std::env::var("SUSLIK_DIR").expect("SUSLIK_DIR env var must be set"))
+        .env("RUSLIC_OPTIMISTICALLY_ALLOW_PRIVATE_TYPES", "true")
+        .env("RUSLIC_FAIL_ON_UNSYNTH", "false")
+        .env("RUSLIC_SUBST_RESULT", "true")
+        .status()
+        .expect("could not run cargo");
+
+    if exit_status.success() {
+        Ok(())
+    } else {
+        Err(exit_status.code().unwrap_or(-1))
+    }
+}
+
+
+pub fn get_rust_toolchain_channel() -> String {
+    #[derive(Deserialize)]
+    struct RustToolchainFile {
+        toolchain: RustToolchain,
+    }
+
+    #[derive(Deserialize)]
+    struct RustToolchain {
+        channel: String,
+        #[allow(dead_code)]
+        components: Option<Vec<String>>,
+    }
+
+    let content = include_str!("../../rust-toolchain.toml");
+    // Be ready to accept TOML format
+    // See: https://github.com/rust-lang/rustup/pull/2438
+    if content.starts_with("[toolchain]") {
+        let rust_toolchain: RustToolchainFile =
+            toml::from_str(content).expect("failed to parse rust-toolchain file");
+        rust_toolchain.toolchain.channel
+    } else {
+        content.trim().to_string()
+    }
+}
